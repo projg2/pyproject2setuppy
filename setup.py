55f50d18c7100c588f6452e0ebd351136d5ed85d
Michał Górny <mgorny@gentoo.org>
2019-12-22 13:53:29 +0100

    Fix fileencoding in setup.py
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# pyproject2setup.py -- cheap builder for pyproject-based build systems
 # vim:se fileencoding=utf-8 :
+# pyproject2setup.py -- cheap builder for pyproject-based build systems
 # (c) 2019 Michał Górny
 # 2-clause BSD license
 
